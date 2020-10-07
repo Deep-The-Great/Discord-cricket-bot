021b6dd0a2beb8f04d426d82c9508f16d16d7897
Void <52120619+Shrikar-Kota@users.noreply.github.com>
2020-10-07 11:22:54 +0530

    Update select_team.py

--- cogs/select_team.py
+++ cogs/select_team.py
@@ -9,12 +9,7 @@ import json
 import math
 db_client=pymongo.MongoClient(os.getenv("DB_URL"))
 db1_client=pymongo.MongoClient(os.getenv("DB2_URL"))
-db_name=db1_client["Challenge"]
-db_collection=db_name['Data']
-db1_name=db1_client['Running_matches']
-db1_collection=db1_name['data']
-db2_name=db_client["about"]
-db2_collection=db2_name["data"]
+
 class select_team(commands.Cog):
 	def __init__(self,bot):
         	self.bot=bot
